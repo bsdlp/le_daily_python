81e77b0f8aeacdcbd91a3711ec362d720aacf83d
Jon Chen <bsd@voltaire.sh>
2013-11-03 23:59:12 -0500

    configparser

--- thedailypython.py
+++ thedailypython.py
@@ -7,5 +7,24 @@ import requests
 
 url = 'http://tdp.me/v1'
 
+def search_tdp_config(**kwargs):
+    """
+    Look for configuration and parse it.
+    """
+    api_key = None
+
+    if 'TDP_CONFIG' in os.environ:
+        paths = [os.environ['TDP_CONFIG']]
+    else:
+        paths = ['/etc/tdp.cfg', '~/.tdp']
+
+    paths = [os.path.expandvars(p) for p in paths]
+    paths = [os.path.expanduser(p) for p in paths]
+
+    cfg = configparser.RawConfigParser()
+    cfg.read(paths)
+
+    if cfg.has_section('Credentials'):
+        api_key = cfg.get('Credentials', 'api_key')
 
 headers = {'X-Access-Token': api_key}
